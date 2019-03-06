7abc248e0e27580727d123d4fa270d8b86995e78
Bob Nadler <bnadlerjr@gmail.com>
2019-03-06 18:22:39 -0500

    Fix string

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -33,7 +33,7 @@ if [ ! -e $NODE_VERSION_NUM ] ; then
     rm -r node-v$NODE_VERSION_NUM && \
     apt-get update && apt install apt-transport-https && \
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && \
-    echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list && \
+    echo 'deb https://dl.yarnpkg.com/debian/ stable main' | tee /etc/apt/sources.list.d/yarn.list && \
     apt-get update && apt-get install yarn"
 fi
 
