8e3871caebdee2a17e600d7bddf11e0e5745d7e4
Bob Nadler <bnadlerjr@gmail.com>
2019-03-06 17:40:53 -0500

    Forgot about apt-transport-https

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -31,6 +31,7 @@ if [ ! -e $NODE_VERSION_NUM ] ; then
     make install && \
     cd .. && \
     rm -r node-v$NODE_VERSION_NUM && \
+    apt-get update && apt install apt-transport-https && \
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && \
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list && \
     apt-get update && apt-get install yarn"
