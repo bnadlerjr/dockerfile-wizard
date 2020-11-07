298719bc3c95f833cdb063850fa4aa501d8d17f6
Bob Nadler <bnadlerjr@gmail.com>
2020-11-07 10:42:47 -0500

    Fix Java install
    According to AskUbuntu[1] the PPA has been discontinued. This blog
    post[2] recommends the workaround I used.
    
    1: https://askubuntu.com/questions/790671/oracle-java8-installer-no-installation-candidate
    2: https://tecadmin.net/install-oracle-java-11-ubuntu-18-04-bionic/

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -62,7 +62,7 @@ RUN if [ \$(grep 'VERSION_ID="8"' /etc/os-release) ] ; then \\
     apt-get update && \\
     echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \\
     echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections && \\
-    apt-get -y install oracle-java8-installer \\
+    apt-get -y install oracle-java8-installer-local \\
 ; elif [ \$(grep 'VERSION_ID="16.04"' /etc/os-release) ] ; then \\
     apt-get update && \\
     apt-get --force-yes -y install software-properties-common python-software-properties && \\
@@ -70,7 +70,7 @@ RUN if [ \$(grep 'VERSION_ID="8"' /etc/os-release) ] ; then \\
     apt-get update && \\
     echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \\
     echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections && \\
-    apt-get -y install oracle-java8-installer \\
+    apt-get -y install oracle-java8-installer-local \\
 ; fi
 EOF
 fi
